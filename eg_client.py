c80058a2adceddf72762a3701279bf87e984f323
BerlinTam <tam.eastley@gmail.com>
2013-09-20 13:50:07 +0200

    Add usage example

--- eg_client.py
+++ eg_client.py
@@ -6,10 +6,18 @@ def send_event(url, api_token, title):
     """Send an event to the event girl with the given url, the api_token and
     the title and return the result of the request.
 
-    Parameters:
-        url - the url of your eventgirl instance.
-        api_token - the token you received.
-        title - the actual title of the event.
+    :param url: the url of your eventgirl instance.
+    :param api_token: the token you received.
+    :param title: the actual title of the event.
+
+    Usage::
+
+        >>> import eg_client
+        >>> eg_client.send_event(
+                url="http://yoururl.com/incoming_events.json",
+                api_token="your_api_token",
+                title="Your Title"
+            )
     """
     payload = {
         "title": title,
